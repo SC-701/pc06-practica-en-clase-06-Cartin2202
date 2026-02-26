77b88510e38f2f47b40b0b6cc6fb130b9b07798d
Cartin2202 <fabricio.cartin.arias@hotmail.com>
2026-02-25 19:00:14 -0600

    commit

---
+++ Vehiculo.API/Servicios/RevisionServicio.cs
@@ -0,0 +1,36 @@
+﻿using Abstracciones.Interfaces.Reglas;
+using Abstracciones.Interfaces.Servicios;
+using Abstracciones.Modelos.Servicios.Registro;
+using Abstracciones.Modelos.Servicios.Revision;
+using System.Text.Json;
+
+namespace Servicios
+{
+    public class RevisionServicio : IRevisionServicio
+    {
+
+        private readonly IConfiguracion _configuracion;
+        private readonly IHttpClientFactory _httpclient;
+
+        public RevisionServicio(IConfiguracion configuracion, IHttpClientFactory httpclient)
+        {
+            _configuracion = configuracion;
+            _httpclient = httpclient;
+        }
+
+        public async Task<Revision> Obtener(string placa)
+        {
+            var endPoint = _configuracion.ObtenerMetodo("ApiEndPointsRevision", "ObtenerRevision");
+            var servicioRegistro = _httpclient.CreateClient("ServicioRevision");
+            var respuesta = await servicioRegistro.GetAsync(string.Format(endPoint, placa));
+            respuesta.EnsureSuccessStatusCode();
+            var resultado = await respuesta.Content.ReadAsStringAsync();
+            var opciones = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var resultadoDeserializado = JsonSerializer.Deserialize<List<Revision>>(resultado, opciones);
+            return resultadoDeserializado.FirstOrDefault();
+        }
+    }
+}
